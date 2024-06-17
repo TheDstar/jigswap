d73b7300be4c7679dbf4ca032408a3baf9a005af
Martin Perret-Pucheu <martinperretp@gmail.com>
2024-06-18 01:28:04 +0200

    Update app.html and added signup.php stylesheets

---
+++ signup.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="refresh" content="5" >
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="viewport" content="width=device-width, viewport-fit=cover">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <title>Inscription - Jigswap</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/signup.css">
+    <link rel="manifest" href="manifest.json">
+    <link href="assets/fontawesome/css/fontawesome.css" rel="stylesheet">
+    <link href="assets/fontawesome/css/brands.css" rel="stylesheet">
+    <link href="assets/fontawesome/css/solid.css" rel="stylesheet">
+</head>
+<body>
+    <div class="signup-container">
+        <div class="signup-container__top">
+            <h1>Inscription (1/3)</h1>
+        </div>
+    </div>
+</body>
+</html>
