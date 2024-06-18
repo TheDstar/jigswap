564654db711a6f5a0135adbeef8171a1d1438c29
Martin Perret-Pucheu <martinperretp@gmail.com>
2024-06-19 01:32:32 +0200

    Test camera permissions

---
+++ assets/js/permissions.js
@@ -0,0 +1,16 @@
+// Vérifier si le navigateur prend en charge la fonctionnalité de la caméra
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    // Demander l'autorisation d'accéder à la caméra
+    navigator.mediaDevices.getUserMedia({ video: true })
+        .then(function(stream) {
+            // L'utilisateur a autorisé l'accès à la caméra, rediriger vers camera.html
+            window.location.href = 'camera.html';
+        })
+        .catch(function(error) {
+            // L'utilisateur a refusé l'accès à la caméra ou une erreur s'est produite
+            document.querySelector('.errorMessage').innerHTML = error;
+        });
+} else {
+    // Le navigateur ne prend pas en charge la fonctionnalité de la caméra
+    console.error('Le navigateur ne prend pas en charge la caméra.');
+}
