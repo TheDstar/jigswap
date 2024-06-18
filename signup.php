e0d03108eb05af9283bf337065a16b26316df084
Martin Perret-Pucheu <martinperretp@gmail.com>
2024-06-18 03:11:31 +0200

    change signup.php

--- signup.php
+++ signup.php
@@ -1,4 +1,59 @@
+<?php
 
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    require_once __DIR__ . '/vendor/autoload.php';
+    session_start();
+
+    // Charger les variables d'environnement depuis le fichier .env
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+
+    // Connexion à la base de données
+    $servername = $_ENV['DB_HOST'];
+    $username = $_ENV['DB_USER'];
+    $password = $_ENV['DB_PASSWORD'];
+    $dbname = $_ENV['DB_NAME'];
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Vérifier la connexion
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Récupérer les données du formulaire
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $confirm_password = $_POST['confirm_password'];
+
+    // Vérifier si le mot de passe et la confirmation sont identiques
+    if ($password !== $confirm_password) {
+        $_SESSION['chosen_username'] = $username;
+        $_SESSION['chosen_password'] = $password;
+        $_SESSION['chosen_confirm_password'] = $confirm_password;
+        header('Location: signup.php?e=password');
+        exit();
+    } else {
+        // Hacher le mot de passe
+        $password = password_hash($password, PASSWORD_DEFAULT);
+        
+        if($_SESSION['chosen_username']) {
+            unset($_SESSION['chosen_username']);
+        };
+        if($_SESSION['chosen_password']) {
+            unset($_SESSION['chosen_password']);
+        };
+        if($_SESSION['chosen_confirm_password']) {
+            unset($_SESSION['chosen_confirm_password']);
+        };
+
+        $_SESSION['valid_username'] = $username;
+        $_SESSION['valid_password'] = $password;
+
+        header('Location: signup2.php');
+    }
+};
+?>
 
 <!DOCTYPE html>
 <html lang="fr">
@@ -23,19 +78,22 @@
 
         <div class="signup-container__body">
             <form action="signup.php" method="post">
+            <?php if(isset($_GET['e']) && $_GET['e'] === "password") { session_start(); } ?>
                 <div class="signup-container-form">
                     <div class="signup-form__group">
                         <label for="username">Pseudonyme</label>
-                        <input type="text" name="username" id="username" required>
+                        <input type="text" name="username" id="username" <?php if(isset($_GET['e']) && $_GET['e'] === "password") { ?>value="<?= $_SESSION['chosen_username'] ?>"<?php } ?> required>
                         <p id="error-message"></p>
                     </div>
                     <div class="signup-form__group">
                         <label for="password">Mot de passe</label>
-                        <input type="password" name="password" required>
+                        <input type="password" name="password" <?php if(isset($_GET['e']) && $_GET['e'] === "password") { ?>value="<?= $_SESSION['chosen_password'] ?>"<?php } ?> required>
+                        <?php if(isset($_GET['e']) && $_GET['e'] === "password") { ?><p id="error-message">Les mots de passe ne correspondent pas.</p><?php } ?>
                     </div>
                     <div class="signup-form__group">
                         <label for="confirm_password">Confirme le mot de passe</label>
-                        <input type="password" name="confirm_password" required>
+                        <input type="password" name="confirm_password" <?php if(isset($_GET['e']) && $_GET['e'] === "password") { ?>value="<?= $_SESSION['chosen_confirm_password'] ?>"<?php } ?> required>
+                        <?php if(isset($_GET['e']) && $_GET['e'] === "password") { ?><p id="error-message">Les mots de passe ne correspondent pas.</p><?php } ?>
                     </div>
                 </div>
                 <button type="submit">Suivant ></button>
